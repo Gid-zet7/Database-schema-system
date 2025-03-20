a372ad3c0b2b7e1f65514ac2b878f7bd4424f296
GideonAdonteng <adontenggideon57@gmail.com>
2025-03-20 16:48:36 -0700

    Integrated Kinde authentication for secure user login and session management

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <LoginLink>Sign in</LoginLink>
+      <RegisterLink>Sign up</RegisterLink>
+    </div>
+  );
 }
